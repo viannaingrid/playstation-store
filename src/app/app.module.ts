8189fb48be33c4f89a0faf7cafe0b0140539a309
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-04-24 09:01:00 -0300

    :construction: add components card and menu-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
