8cc1a7943db79c94d8e7b45fa2371785b21af71e
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-04-25 23:42:24 -0300

    `Updated routing configuration and navigation links to use Angular router`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ColecoesComponent } from './pages/colecoes/colecoes.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
 
 const routes: Routes = [
-	/* {
-		path:'',
-		component: ,
-	},
 	{
 		path:'',
-		component:,
-	} */
+		component: HomeComponent,
+		pathMatch: 'full'
+	},
+	{
+		path:'colecoes',
+		component: ColecoesComponent,
+	},
+	{
+		path:'ofertas',
+		component: OfertasComponent
+	}
 ];
 
 @NgModule({
